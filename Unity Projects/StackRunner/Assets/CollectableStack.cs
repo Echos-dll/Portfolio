5d93afe62f89ef3747748da9b57233fcf04b3989
Ekrem İbram <talanov1@gmail.com>
2022-03-29 11:48:47 +0300

    StackRunner

--- Unity Projects/StackRunner/Assets/CollectableStack.cs
+++ Unity Projects/StackRunner/Assets/CollectableStack.cs
@@ -1,28 +1,44 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectableStack : MonoBehaviour
 {
     [SerializeField] private GameObject stackParent;
     [SerializeField] private GameObject stackItemPrefab;
-    
+
+    private List<GameObject> stackedItems = new List<GameObject>();
     private int _stackNumber = 0 ;
-    
-    // Start is called before the first frame update
-    void Start()
+    private Material currentMaterial;
+
+    private void Start()
     {
-        
+        currentMaterial = new Material(Shader.Find("Default-Material"));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveItem()
     {
-        
+        Debug.Log("Removed Item");
     }
 
     public void AddItem()
     {
         var item = Instantiate(stackItemPrefab, stackParent.transform);
-        item.transform.position = transform.position + Vector3.down * _stackNumber;
+        item.GetComponent<Renderer>().material = currentMaterial;
+        item.transform.position = stackParent.transform.position + Vector3.up / 2  * _stackNumber;
+        StackedItems.Add(item);
         _stackNumber += 1;
+    }
+    
+    public List<GameObject> StackedItems
+    {
+        get => stackedItems;
+        set => stackedItems = value;
+    }
+
+    public Material CurrentMaterial
+    {
+        get => currentMaterial;
+        set => currentMaterial = value;
     }
 }
